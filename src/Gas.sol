e964be61c45752e0f0efa869b298ec5a25b0269d
Tahlil <bsse0803@iit.du.ac.bd>
2023-05-08 21:39:39 +0600

    Remove unnecessary functions and variables

--- src/Gas.sol
+++ src/Gas.sol
@@ -13,39 +13,13 @@ contract GasContract is Ownable, Constants {
     uint256 public totalSupply = 0; // cannot be updated
     uint256 public paymentCounter = 0;
     mapping(address => uint256) public balances;
-    uint256 public tradePercent = 12;
+
     address public contractOwner;
     uint256 public tradeMode = 0;
-    mapping(address => Payment[]) public payments;
+
     mapping(address => uint256) public whitelist;
     address[5] public administrators;
-    bool public isReady = false;
-    enum PaymentType {
-        Unknown,
-        BasicPayment,
-        Refund,
-        Dividend,
-        GroupPayment
-    }
-    PaymentType constant defaultPayment = PaymentType.Unknown;
-
-    History[] public paymentHistory; // when a payment was updated
-
-    struct Payment {
-        PaymentType paymentType;
-        uint256 paymentID;
-        bool adminUpdated;
-        string recipientName; // max 8 characters
-        address recipient;
-        address admin; // administrators address
-        uint256 amount;
-    }
-
-    struct History {
-        uint256 lastUpdate;
-        address updatedBy;
-        uint256 blockNumber;
-    }
+  
     mapping(address => uint256) public isOddWhitelistUser;
     
     struct ImportantStruct {
@@ -62,12 +36,7 @@ contract GasContract is Ownable, Constants {
 
     event supplyChanged(address indexed, uint256 indexed);
     event Transfer(address recipient, uint256 amount);
-    event PaymentUpdated(
-        address admin,
-        uint256 ID,
-        uint256 amount,
-        string recipient
-    );
+   
     event WhiteListTransfer(address indexed);
 
     constructor(address[] memory _admins, uint256 _totalSupply) {
@@ -100,53 +69,11 @@ contract GasContract is Ownable, Constants {
         }
         return admin;
     }
-
-    function getPaymentHistory()
-        external
-        payable
-        returns (History[] memory paymentHistory_)
-    {
-        return paymentHistory;
-    }
-
     
 
     function balanceOf(address _user) external view returns (uint256 balance_) {
         uint256 balance = balances[_user];
         return balance;
-    }
-
-    function getTradingMode() public view returns (bool mode_) {
-        bool mode = false;
-        if (tradeFlag == 1 || dividendFlag == 1) {
-            mode = true;
-        } else {
-            mode = false;
-        }
-        return mode;
-    }
-
-
-    function addHistory(address _updateAddress, bool _tradeMode)
-        public
-        returns (bool status_, bool tradeMode_)
-    {
-        History memory history;
-        history.blockNumber = block.number;
-        history.lastUpdate = block.timestamp;
-        history.updatedBy = _updateAddress;
-        paymentHistory.push(history);
-      
-        return (true, _tradeMode);
-    }
-
-    function getPayments(address _user)
-        external
-        view
-        returns (Payment[] memory payments_)
-    {
-        if(_user != address(0)) revert();
-        return payments[_user];
     }
 
     function transfer(
@@ -159,47 +86,8 @@ contract GasContract is Ownable, Constants {
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
         emit Transfer(_recipient, _amount);
-        Payment memory payment;
-        payment.admin = address(0);
-        payment.adminUpdated = false;
-        payment.paymentType = PaymentType.BasicPayment;
-        payment.recipient = _recipient;
-        payment.amount = _amount;
-        payment.recipientName = _name;
-        payment.paymentID = ++paymentCounter;
-        payments[msg.sender].push(payment);
-        bool[] memory status = new bool[](tradePercent);
-        for (uint256 i = 0; i < tradePercent; i++) {
-            status[i] = true;
-        }
-        return (status[0] == true);
-    }
-
-    function updatePayment(
-        address _user,
-        uint256 _ID,
-        uint256 _amount,
-        PaymentType _type
-    ) external {
-        if(!checkForAdmin(msg.sender) || _ID == 0 || _user == address(0)) revert();
-
-        for (uint256 ii = 0; ii < payments[_user].length; ii++) {
-            Payment memory payment = payments[_user][ii];
-            if (payment.paymentID == _ID) {
-                payment.adminUpdated = true;
-                payment.admin = _user;
-                payment.paymentType = _type;
-                payment.amount = _amount;
-                bool tradingMode = getTradingMode();
-                addHistory(_user, tradingMode);
-                emit PaymentUpdated(
-                    msg.sender,
-                    _ID,
-                    _amount,
-                    payment.recipientName
-                );
-            }
-        }
+        
+        return true;
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier)
@@ -230,17 +118,8 @@ contract GasContract is Ownable, Constants {
         emit WhiteListTransfer(_recipient);
     }
 
-
     function getPaymentStatus(address sender) public returns (bool, uint256) {        
         return (whiteListStruct[sender].paymentStatus, whiteListStruct[sender].amount);
     }
 
-    receive() external payable {
-        payable(msg.sender).transfer(msg.value);
-    }
-
-
-    fallback() external payable {
-         payable(msg.sender).transfer(msg.value);
-    }
 }
